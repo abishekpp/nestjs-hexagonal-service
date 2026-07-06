11baaadadccc4d62ceb19115527292cc11a4e7e2
Abhishek <abhishekkanichery@gmail.com>
2026-07-06 11:50:00 +0530

    feat: wire transmittal providers in transmittal module

--- src/modules/transmittals/transmittals.module.ts
+++ src/modules/transmittals/transmittals.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { TransmittalsGrpcController } from './adapters/in/grpc/controllers/transmittals-grpc.controller';
+import { PrismaModule } from 'src/infrastructure/database/prisma/prisma.module';
+import { CreateTransmittalUseCase } from './application/use-cases/create-transmittal.use-case';
+import { GetTransmittalByIdUseCase } from './application/use-cases/get-transmittal-by-id.use-case';
+import { TRANSMITTAL_REPOSITORY_PORT } from './ports/out/transmittal.repository.port';
+import { PrismaTransmittalRepositoryAdapter } from './adapters/out/persistence/prisma/prisma-transmittal.repository.adapter';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule],
   controllers: [TransmittalsGrpcController],
-  providers: [],
+  providers: [
+    CreateTransmittalUseCase,
+    GetTransmittalByIdUseCase,
+    {
+      provide: TRANSMITTAL_REPOSITORY_PORT,
+      useClass: PrismaTransmittalRepositoryAdapter,
+    },
+  ],
 })
 export class TransmittalsModule {}
