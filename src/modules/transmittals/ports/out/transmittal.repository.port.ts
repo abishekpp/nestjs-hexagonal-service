b0dc17bc309c9caae9cb30f13b7150e84e57f2d0
Abhishek <abhishek@bluecast.tech>
2026-07-10 08:39:30 +0530

    feat: utilize abstract repository ports and repositories to avoid duplications on domain

--- src/modules/transmittals/ports/out/transmittal.repository.port.ts
+++ src/modules/transmittals/ports/out/transmittal.repository.port.ts
@@ -1,9 +1,9 @@
 import { BaseRepositoryPort } from 'src/common/ports/base-repository.port';
-import { Transmittal } from '../../domain/entities/transmittal.entity';
+import { Transmittal as TransmittalDomainEntitiy } from '../../domain/entities/transmittal.entity';
 
 export const TRANSMITTAL_REPOSITORY_PORT = Symbol('TRANSMITTAL_REPOSITORY_PORT');
 
-export interface TransmittalRepositoryPort extends BaseRepositoryPort<Transmittal> {
+export interface TransmittalRepositoryPort extends BaseRepositoryPort<TransmittalDomainEntitiy> {
   // If you have additonal and this module specific port add here.
 
   existByProjectAndSubject(projectId: string, subject: string): Promise<boolean>;
